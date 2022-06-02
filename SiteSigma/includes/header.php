7eab3baaabf077917b5ee2ad47370c225c289d26
Gabriel Sefrim <92944219+Sefrim06@users.noreply.github.com>
2022-06-02 10:49:46 -0300

    Add files via upload

---
+++ SiteSigma/includes/header.php
@@ -0,0 +1,10 @@
+<header>
+<nav>
+    <ul> 
+        <li><a href="index.php">Home</a></li>
+        <li><a href="artigo.php">Empresa</a></li>
+        <li><a href="produtos-categoria.php">Produtos</a></li>
+        <li><a href="contato.php">Contato</a></li>
+    </ul>
+</nav>
+</header>
---
+++ SiteSigma/index.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once "./includes/head.php";
+include_once "./includes/header.php";
+?>
+
+<main>
+
+<script src="script.js"></script> 
+<?php
+
+include_once "./includes/footer.php";
+?>
+
